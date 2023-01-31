08114b923461fc3ac946d32763e2ea9abc37c6f8
Yuriy Voshchepynets <yuriy.voshch@gmail.com>
2023-01-31 16:06:03 +0100

    use published crate

--- src/bin/show_posts.rs
+++ src/bin/show_posts.rs
@@ -1,19 +1,6 @@
-use std::env::args;
-
 use diesel::prelude::*;
 use lunatic::Mailbox;
 use sqlite_lunatic_example::{models::Post, *};
-
-// for testing setting of custom allocator
-// via host function set_custom_guest_allocator
-#[no_mangle]
-pub fn __test_set_guest_allocator(len: u32) -> *mut u8 {
-    let mut buf = Vec::with_capacity(len as usize);
-    println!("CUSTOM ALLOCATOR USED for {} bytes", len);
-    let ptr = buf.as_mut_ptr();
-    std::mem::forget(buf);
-    ptr
-}
 
 #[lunatic::main]
 fn main(_: Mailbox<()>) {
